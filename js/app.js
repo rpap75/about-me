68849b0ca11e4826428b606c3daf499dce4c8c82
Colter-Wilson <itscolter@gmail.com>
2022-10-27 18:48:53 -0400

    functions added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 let username = prompt('Hello, What is your name?');
 alert(`Thanks for stopping by my site, ${username}! If you are up for the challenge here are some questions!`);
 
+function skyDiving() {
 let Q1 = prompt('Have I ever been skydiving?').toUpperCase();
 if (Q1 === 'YES' || Q1 === 'Y') {
   //console.log('Correct!');
@@ -10,9 +11,12 @@ if (Q1 === 'YES' || Q1 === 'Y') {
 } else if (Q1 === 'NO' || Q1 === 'N') {
   //console.log('Incorrect!');
   alert('Incorrect! I have been skydiving');
-  score++;
+  // score++;
+} 
 }
+skyDiving();
 
+function pets() {
 let Q2 = prompt('Do I Have Any Pets?').toUpperCase();
 if (Q2 === 'YES' || Q2 === 'Y') {
   //console.log('Correct! I have one cat.');
@@ -20,9 +24,12 @@ if (Q2 === 'YES' || Q2 === 'Y') {
 } else if (Q2 === 'NO' || Q2 === 'N') {
   //console.log('Incorrect!');
   alert('Incorrect! I have 1 cat.');
-  score++;
+  // score++;
 }
+}
+pets();
 
+function scubaDiving() {
 let Q3 = prompt('Have I ever been scuba diving?').toUpperCase();
 if (Q3 === 'YES' || Q3 === 'Y') {
   //console.log('Incorrect!');
@@ -30,9 +37,12 @@ if (Q3 === 'YES' || Q3 === 'Y') {
 } else if (Q3 === 'NO' || Q3 === 'N') {
   //console.log('Correct!');
   alert('Correct! But I do want to go.');
-  score++;
+  // score++;
 }
+}
+ skyDiving();
 
+function movie() {
 let Q4 = prompt('Do I like the new Dune movie?').toUpperCase();
 if (Q4 === 'YES' || Q4 === 'Y') {
   //console.log('Correct!');
@@ -40,9 +50,12 @@ if (Q4 === 'YES' || Q4 === 'Y') {
 } else if (Q4 === 'NO' || Q4 === 'N') {
   //console.log('Incorrect');
   alert('Incorrect! It is amazing!');
-  score++;
+  // score++;
 }
+}
+movie();
 
+function hungry() {
 let Q5 = prompt('Am I hungry?').toUpperCase();
 if (Q5 === 'YES' || Q5 === 'Y') {
   //console.log('Correct!');
@@ -50,9 +63,12 @@ if (Q5 === 'YES' || Q5 === 'Y') {
 } else if (Q5 === 'NO' || Q5 === 'N') {
   //console.log('Incorrect!')
   alert('Incorrect! I am always hungry!');
-  score++;
+  // score++;
 }
+}
+hungry();
 
+function guessNum() {
 let myNum = Math.floor(Math.random() * 14) + 1;
 let guesses = 4;
 
@@ -67,7 +83,7 @@ while (guesses > 0) {
   } else if (parseInt(guessNum) === myNum) {
     alert('Correct');
     console.log('Correct');
-    score++;
+    //score++;
     break;
   }
   guesses--;
@@ -75,7 +91,10 @@ while (guesses > 0) {
     alert(`the correct answer was ${myNum}`);
   }
 }
+}
+guessNum();
 
+function music() {
 let answers = ['panic at the disco', 'queen', 'george ezra', 'explosions in the sky', 'green day'];
 let secGuesses = 6
 
@@ -85,14 +104,18 @@ for (let i = 0; i < 6; i++) {
   if (guessBand === 'queen' || guessBand === 'panic at the disco' || guessBand === 'george ezra' || guessBand === 'explosions in the sky' || guessBand === 'green day') {
     alert('Correct');
     console.log('Correct');
-    score++;
+    //score++;
     break;
   } else {
     alert('Incorrect');
     console.log('Incorrect');
   }
-} alert('Here were the correct answers, Panic At The Disco, Queen, George Ezra, Explosions In The Sky, and Green Day');
+}
+}
+music();
+ alert('Here were the correct answers, Panic At The Disco, Queen, George Ezra, Explosions In The Sky, and Green Day');
 
 alert(`Thanks for stopping by and answering some questions! Have a great rest of your day ${username}!`);
+
 
 // alert(score: ${ score } / 2');
